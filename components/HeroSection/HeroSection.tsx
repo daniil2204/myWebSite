28576d3697daf12b43aa55a8f26248b458239050
Daniil Drahin <dragindaniilmeizu@gmail.com>
2024-06-30 14:00:57 +0300

    cv added

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -40,7 +40,7 @@ const HeroSection = () => {
             </Link>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
               <a
-                href="/images/heroImage.png"
+                href="/Drahin Daniil CV.pdf"
                 download
                 className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"
               >
